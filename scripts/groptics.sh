d30c60963fc305b40eb59ecc223ac64b669b4956
Gernot Maier <gernot.maier@desy.de>
2024-01-10 08:34:24 +0100

    groptics

--- scripts/groptics.sh
+++ scripts/groptics.sh
@@ -1,23 +1,74 @@
 #!/bin/bash
 # Helper functions for GrOptics; called from prepare_production.sh
 
-# get extinction file generated with MODTRAN
-get_extinction_file()
+#####################################################################
+# corsikaIOreader command
+#
+# queff = 0.5: 50% of all photons are (randomly) discared.
+#              This is taken into account in the queff, and
+#              reflectivity part of the simulations.
+# abs:         extinction file for Cherenkov photon scattering
+#              and absorption (note that requirement of "M5" in the
+#              file name of the extinction file)
+# cfg:         telescope model file. Used only to map the telescopes
+#              from CORSIKA to GrOptics (more telescopes might have
+#              been simulated in CORSIKA than are used in GrOptics).
+#
+# Note: corsikaIOreader requires also the atmprof file as
+# ./data/atmprof${ATMOSPHERE}.dat from the path the executable is
+# called.
+prepare_corsikaIOreader()
 {
-    ATMOSPHERE="$1"
-    if [[ $ATMOSPHERE == "61" ]]; then
-        echo "Ext_results_VWinter_3_2_6.M5.txt"
-    elif [[ $ATMOSPHERE == "62" ]]; then
-        echo "Ext_results_VSummer_6_1_6.M5.txt"
-    else
-        echo "INVALID_EXTINCTION_FILE_FOR_$ATMOSPHERE"
-    fi
+    CORSIKA_IO_READER="../corsikaIOreader/corsikaIOreader \
+        -queff 0.50\
+        -cors ${CORSIKA_FILE} \
+        -seed ${RUN_NUMBER} -grisu stdout \
+        -abs /workdir/GrOptics/data/${EXTINCTION_FILE} \
+        -cfg /workdir/GrOptics/data/${TELESCOPE_MODEL}"
+    echo "$CORSIKA_IO_READER"
 }
 
+
+#####################################################################
+# generate GrOptics pilot file
+#
+# The pilot file is used to configure the GrOptics simulation.
+#
+# WOBBLE:     wobble offset and direction in degrees
+# ARRAYCONFIG: telescope model file
+# SEED:       seed for the random number generator (0=machine clock)
+generate_groptics_pilot_file()
+{
+    LOG_DIR="$1"
+    RUN_NUMBER="$2"
+
+    PILOT="${LOG_DIR}/pilot_${RUN_NUMBER}.dat"
+    rm -f "$PILOT"
+    touch "$PILOT"
+
+    {
+        echo "* FILEOUT photonLocation.root allT T 0"
+        echo "* NSHOWER -1 -1"
+        echo "* WOBBLE 0.5 0.0 0.0 90.0"
+        echo "* ARRAYCONFIG ./data/VERITAS_NewArray.cfg"
+        echo "* SEED 0"
+        echo "* DEBUGBRANCHES 1"
+    } >> "$PILOT"
+    echo "/workdir/external/log/$(basename "$PILOT")"
+}
+
+#####################################################################
 # preparation of GrOptics containers
 #
 # Unfortunately quite fine tuned due to directory requirements of
-# corsikaIOreader and grOptics.
+# corsikaIOreader and grOptics:
+#
+# /workdir/external/corsika: directory with CORSIKA (input) files
+# /workdir/external/log:     directory for run scripts and pilot files
+# /workdir/external/groptics:directory with GrOptics (output) files
+# /workdir/GrOptics/Config:  directory into which model files
+#                            (atmosphere, telescope model) are copied.
+#
 prepare_groptics_containers()
 {
     DATA_DIR="$1"
@@ -31,12 +82,13 @@ prepare_groptics_containers()
     mkdir -p "$TMP_CONFIG_DIR"
 
     # copy file for atmospheric extinction (corsikaIOreader required "M5" in file name)
-    EXTINCTION_FILE=$(get_extinction_file "$3")
-    cp -f "$(dirname "$0")"/../config/ATMOSPHERE/"$EXTINCTION_FILE" "${TMP_CONFIG_DIR}/extinction_model.M5.dat"
+    cp -f "$(dirname "$0")"/../config/ATMOSPHERE/"$EXTINCTION_FILE" "${TMP_CONFIG_DIR}/"
     # copy file for atmospheric profile (corsikaIOreader expect it in the /workdir/external/groptics/data directory)
-    cp -f "$(dirname "$0")"/../config/ATMOSPHERE/atmprof"$ATMOSPHERE".dat "${TMP_CONFIG_DIR}/atmprof${ATMOSPHERE}.dat"
+    cp -f "$(dirname "$0")"/../config/ATMOSPHERE/atmprof"$ATMOSPHERE".dat "${TMP_CONFIG_DIR}/"
     # copy file for telescope model
-    cp -f "$(dirname "$0")"/../config/TELESCOPE_MODEL/"$TELESCOPE_MODEL" "${TMP_CONFIG_DIR}/telescope_model.dat"
+    cp -f "$(dirname "$0")"/../config/TELESCOPE_MODEL/"$TELESCOPE_MODEL" "${TMP_CONFIG_DIR}/"
+    # copy file for GrOptics configuration
+    cp -f "$(dirname "$0")"/../config/TELESCOPE_MODEL/"$GROPTICS_CONFIG" "${TMP_CONFIG_DIR}/"
 
     # mount directories
     CORSIKA_DATA_DIR="${DATA_DIR}/CORSIKA"
@@ -55,6 +107,7 @@ prepare_groptics_containers()
     fi
 }
 
+#####################################################################
 # generate GrOptics input files and submission scripts
 generate_groptics_submission_script()
 {
@@ -70,13 +123,9 @@ generate_groptics_submission_script()
         CORSIKA_FILE="/workdir/external/corsika/$(basename "$CORSIKA_FILE")"
     fi
 
-    CORSIKA_IO_READER="../corsikaIOreader/corsikaIOreader \
-        -queff 0.50\
-        -cors ${CORSIKA_FILE} \
-        -seed ${RUN_NUMBER} -grisu stdout \
-        -abs /workdir/external/groptics/model_files/extinction_model.M5.dat \
-        -cfg /workdir/external/groptics/model_files/telescope_model.dat"
-    GROPTICS="grOptics -of ${OUTPUT_FILE}.groptics -p ${gro_pilot}"
+    CORSIKA_IO_READER=$(prepare_corsikaIOreader)
+    GROPTICS="./grOptics -of /workdir/external/groptics/$(basename OUTPUT_FILE).groptics \
+     -p $(generate_groptics_pilot_file "$LOG_DIR" "$RUN_NUMBER"))"
 
     echo "#!/bin/bash" > "$FSCRIPT.sh"
     if [[ $VTSSIMPIPE_CONTAINER == "docker" ]]; then
