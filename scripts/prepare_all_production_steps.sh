bb3d1606aab7e13a7a3f4205aa81de5bc410669d
GernotMaier <gernot.maier@desy.de>
2024-01-20 15:30:28 +0100

    prepare all productions

---
+++ scripts/prepare_all_production_steps.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# Prepare all simulation production steps
+
+if [ $# -lt 2 ]; then
+    echo "./prepare_all_production_steps.sh <config file> <corsika input file> [remove previous file (Dangerous!)"
+    exit
+fi
+
+CONFIG="$1"
+INPUT="$2"
+CLEAN_ALL_FILES="$3"
+
+# shellcheck source=/dev/null
+. "$(dirname "$0")"/../env_setup.sh
+# shellcheck source=/dev/null
+. "$CONFIG"
+DIRSUFF="ATM${ATMOSPHERE}/Zd${ZENITH}"
+
+for step in CORSIKA GROPTICS CARE; do
+    if [ ! -z $CLEAN_ALL_FILES ] && [ $CLEAN_ALL_FILES == "clean_all_files" ]; then
+        echo "cleaning $step"
+        rm -rf "$VTSSIMPIPE_LOG_DIR"/"$DIRSUFF"/"$step"
+        rm -rf "$VTSSIMPIPE_DATA_DIR"/"$DIRSUFF"/"$step"
+        rm -rf "$VTSSIMPIPE_DATA_DIR"/"$DIRSUFF"/"${step}_std"
+        rm -rf "$VTSSIMPIPE_DATA_DIR"/"$DIRSUFF"/"${step}_redHV"
+    fi
+    echo "prepare $step"
+    ./prepare_production.sh $step ${CONFIG} ${INPUT}
+done
