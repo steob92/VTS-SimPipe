7b8fa4ea784e2eb081c0f9c7aa88a7862fb45b12
GernotMaier <gernot.maier@desy.de>
2024-02-10 12:05:48 +0100

    v1.1

--- env_setup_template.sh
+++ env_setup_template.sh
@@ -13,10 +13,10 @@ export VTSSIMPIPE_CONTAINER_URL="ghcr.io/veritas-observatory/"
 # apptainers are stored in this directory
 export VTSSIMPIPE_CONTAINER_DIR=vtssimpipe_containe_dir
 # CORSIKA
-export VTSSIMPIPE_CORSIKA_IMAGE="vtsimpipe-corsika:1.0.0"
+export VTSSIMPIPE_CORSIKA_IMAGE="vtsimpipe-corsika:1.1.0"
 # GROPTICS
-export VTSSIMPIPE_GROPTICS_IMAGE="vtsimpipe-groptics:1.0.0"
+export VTSSIMPIPE_GROPTICS_IMAGE="vtsimpipe-groptics:1.1.0"
 # CARE
-export VTSSIMPIPE_CARE_IMAGE="vtsimpipe-care:1.0.0"
+export VTSSIMPIPE_CARE_IMAGE="vtsimpipe-care:1.1.0"
 # MERGEVBF
-export VTSSIMPIPE_MERGEVBF_IMAGE="vtsimpipe-mergevbf:1.0.0"
+export VTSSIMPIPE_MERGEVBF_IMAGE="vtsimpipe-mergevbf:1.1.0"
